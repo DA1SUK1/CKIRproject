e7e4de1d52b5008999059aee5a07521a229049cf
DA1SUK1 <idonly4project@gmail.com>
2018-03-02 20:04:18 +0900

    second commit

--- auto_library_renewal.py
+++ auto_library_renewal.py
@@ -11,15 +11,17 @@ import pdb
 
 def pnu_library_login(driver, id_string, pw_string):
     driver.get("https://lib.pusan.ac.kr/login/")
+    sleep(10)
     driver.find_element_by_id("id").send_keys(id_string)
     driver.find_element_by_id("pw").send_keys(pw_string)
-    sleep(1)
+    sleep(3)
     driver.find_element_by_class_name("btn_login").click()
-    sleep(1)
+    sleep(3)
     
 def get_burrowed_books(driver):
     driver.get("https://pulip.pusan.ac.kr/mylibrary/Circulation.ax")
-    sleep(1)
+    sleep(10)
+    #pdb.set_trace()
     soup = BeautifulSoup(driver.page_source, "html.parser")
     for book in soup.findAll("tr", {"class":re.compile("tbRecord*")}):
         renewal_date = tuple(book.findAll("td")[4].get_text().strip().split('/'))
@@ -37,13 +39,13 @@ def is_renewal_date(book):
 
 def renewal(driver, book):
     driver.get("https://pulip.pusan.ac.kr/mylibrary/Circulation.ax")
-    sleep(1)
+    sleep(10)
     driver.find_element_by_id(book[0]).click()
-    sleep(1)
+    sleep(10)
     Alert(driver).accept()
-    sleep(1)
+    sleep(10)
     Alert(driver).accept()
-    sleep(1)
+    sleep(10)
     
 def write_log(f, isRenewaled):
     today = datetime.today()
@@ -53,15 +55,18 @@ def write_log(f, isRenewaled):
         f.write(str(today.year) + "/" + str(today.month) + "/" + str(today.day) + " " + str(today.hour) + ":" + str(today.minute) + " " + " 1 book not renewal\n")
     
 if __name__=="__main__":
+    geckodriver_path = "/home/gooninn/geckodriver"
+    recordfile_path = "/home/gooninn/renewal_record"
+    userid = sys.argv[1]
+    userpw = sys.argv[2]
+    
     options = Options()
     options.add_argument("--headless")
-    driver = webdriver.Firefox(firefox_options=options, executable_path="/home/goonin/geckodriver")
+    driver = webdriver.Firefox(firefox_options=options, executable_path=geckodriver_path)
     
-    f = open("/home/goonin/Desktop/renewal_record", "a")
+    f = open(recordfile_path, "a")
     
-    #print(sys.argv[1])
-    #print(sys.argv[2])
-    pnu_library_login(driver, sys.argv[1], sys.argv[2])
+    pnu_library_login(driver, userid, userpw)
     
     for book in get_burrowed_books(driver):
         if is_renewal_date(book):
