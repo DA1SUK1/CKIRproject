87cf5f30e86e399f47d781ea1c6fac96477533b0
DA1SUK1 <idonly4project@gmail.com>
2018-04-01 11:43:37 +0900

    seventh commit

--- auto_library_renewal.py
+++ auto_library_renewal.py
@@ -22,14 +22,16 @@ def get_burrowed_books(driver):
     burrowed_books = []
     driver.get("https://pulip.pusan.ac.kr/mylibrary/Circulation.ax") #here needs wait
     sleep(10)
-    #pdb.set_trace()
+    
     soup = BeautifulSoup(driver.page_source, "html.parser")
     for book in soup.findAll("tr", {"class":re.compile("tbRecord*")}):
-        renewal_date = date(*(int(x) for x in book.findAll("td")[4].get_text().strip().split('/')))
-        book_code = book.findAll("td")[1].get_text().strip()
-        is_renewaled = True if book.findAll("td")[5].get_text().strip()[0] == '1' else False
-        #pdb.set_trace()
-        burrowed_books.append({"book_code":book_code, "renewal_date":renewal_date, "is_renewaled":is_renewaled})
+        try:  # to avoid index error by overdue days
+            renewal_date = date(*(int(x) for x in book.findAll("td")[4].get_text().strip().split('/')))
+            book_code = book.findAll("td")[1].get_text().strip()
+            is_renewaled = True if book.findAll("td")[5].get_text().strip()[0] == '1' else False
+            burrowed_books.append({"book_code":book_code, "renewal_date":renewal_date, "is_renewaled":is_renewaled})
+        except IndexError:
+            pass
     return sorted(burrowed_books, key = lambda k : k["is_renewaled"], reverse = True) #renewaled books come first
         
 def is_renewal_day(book, today):
@@ -37,6 +39,7 @@ def is_renewal_day(book, today):
     if not hasattr(is_renewal_day, "renewal_to_reduce_fee"):   #for static variable in this function
         is_renewal_day.renewal_to_reduce_fee = False
       
+    #pdb.set_trace()
     if today.date() == book["renewal_date"] and book["is_renewaled"] and not is_renewal_day.renewal_to_reduce_fee:
         is_renewal_day.renewal_to_reduce_fee = True
         return False
@@ -59,7 +62,7 @@ def renewal(driver, book):
     
 if __name__=="__main__":
     geckodriver_path = "/home/gooninn/geckodriver"
-    recordfile_path = "/home/gooninn/renewal_record"
+    recordfile_path = "/home/gooninn/renewal_record2"
     
     today = datetime.today()
     renewaled_book = 0
@@ -69,14 +72,21 @@ if __name__=="__main__":
     userpw = sys.argv[2]
     
     f = open(recordfile_path, "a")
-    
     try:
         options = Options()
         options.add_argument("--headless")
         driver = webdriver.Firefox(firefox_options=options, executable_path=geckodriver_path)
-    
+    except:
+        f.write("problem occured in selenium.\n\n")
+        sys.exit()
+        
+    try:
+        options = Options()
+        options.add_argument("--headless")
+        driver = webdriver.Firefox(firefox_options=options, executable_path=geckodriver_path)
+        
         pnu_library_login(driver, userid, userpw)
-    
+        
         for book in get_burrowed_books(driver):
             if is_renewal_day(book, today):
                 renewal(driver, book)
@@ -84,10 +94,11 @@ if __name__=="__main__":
             else:
                 not_renewaled_book = not_renewaled_book + 1
         
-        driver.quit()
+        #driver.quit()
     except:
-        f.write("crontab worked. but error occured in script.\n")
+        f.write("error occured in script.\n")
     finally:
         f.write(str(today.year) + "/" + str(today.month) + "/" + str(today.day) +  " " + str(today.hour) + ":" + str(today.minute) + " " + str(renewaled_book) + " book renewal\n")
         f.write(str(today.year) + "/" + str(today.month) + "/" + str(today.day) + " " + str(today.hour) + ":" + str(today.minute) + " " + str(not_renewaled_book) + " book not renewal\n\n")
         f.close()
+        driver.quit()
